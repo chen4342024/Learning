0bbe27415679f98d4a1f01f976d644485867d63b
chenjiajia <chenjiajia@vapps.hk>
2016-09-02 17:32:07 +0800

    增加方法

--- learn_js/learn_lib/learn_underscore/underscore-chen/test/collection.spec.js
+++ learn_js/learn_lib/learn_underscore/underscore-chen/test/collection.spec.js
@@ -1,6 +1,5 @@
 describe('each', function () {
 
-
     it('each iterators provide value and iteration count', function () {
         _.each([1, 2, 3], function (num, i) {
             expect(num).toEqual(i + 1);
@@ -45,7 +44,7 @@ describe('each', function () {
         _.each(obj, function () {
             count++;
         });
-        expect(count, 3);
+        expect(count).toBe(3);
     });
 
     it("can reference the original collection from inside the iterator", function () {
@@ -63,7 +62,7 @@ describe('each', function () {
         _.each(null, function () {
             ++answers;
         });
-        expect(answers, 0);
+        expect(answers).toBe(0);
 
         _.each(false, function () {
         });
@@ -81,6 +80,295 @@ describe('each', function () {
 });
 
 
-describe('sortPageListKey', function () {
+describe('map', function () {
+    it('double number', function () {
+        var doubleArray = _.map([1, 2, 3], function (num) {
+            return num * 2;
+        });
+        expect(doubleArray).toEqual([2, 4, 6]);
+    });
+
+    it('can handle null', function () {
+        var nullArr = _.map(null, function (num) {
+            return num * 2;
+        });
+        expect(nullArr).toEqual([]);
+    });
+
+    it('tripled numbers with context', function () {
+        var tripled = _.map([1, 2, 3], function (num) {
+            return num * this.multiplier;
+        }, {multiplier: 3});
+        expect(tripled).toEqual([3, 6, 9]);
+    });
+
+    it('Can use collection methods on Array-likes.', function () {
+        var ids = _.map({length: 2, 0: {id: '1'}, 1: {id: '2'}}, function (n) {
+            return n.id;
+        });
+        expect(ids).toEqual(['1', '2']);
+    })
+});
+
+
+describe("reduce", function () {
+    it('can sum up an array', function () {
+        var sum = _.reduce([1, 2, 3, 4], function (memo, num) {
+            return memo + num;
+        }, 0);
+        expect(sum).toBe(10);
+    });
+
+    it('can reduce with a context object', function () {
+        var context = {multiplier: 3};
+        var sum = _.reduce([1, 2, 3], function (memo, num) {
+            return memo + num * this.multiplier;
+        }, 0, context);
+        expect(sum).toBe(18);
+    });
+
+    it('default initial value', function () {
+        var sum = _.reduce([1, 2, 3], function (memo, num) {
+            return memo + num;
+        });
+        expect(sum).toBe(6);
+    });
+});
+
+describe("reduceRight", function () {
+    it('can perform right folds', function () {
+        var strSum = _.reduceRight(['1', '2', '3', '4'], function (memo, str) {
+            return memo + str;
+        });
+        expect(strSum).toBe('4321');
+    });
+
+    it('can reduce with a context object', function () {
+        var context = {multiplier: 3};
+        var sum = _.reduceRight([1, 2, 3], function (memo, num) {
+            return memo + num * this.multiplier;
+        }, 0, context);
+        expect(sum).toBe(18);
+    });
+
+    it('default initial value', function () {
+        var sum = _.reduceRight([1, 2, 3], function (memo, num) {
+            return memo.concat(num);
+        }, []);
+        expect(sum).toEqual([3, 2, 1]);
+    });
+
+    it('can handle null value', function () {
+        var result = _.reduceRight(null, _.noop, 138);
+        expect(result).toBe(138);
+    });
+
+    it('undefined can be passed as a special case', function () {
+        var result = _.reduceRight([], _.noop, void 0);
+        expect(result).toBe(void 0);
+    });
+});
+
+describe("find", function () {
+    it('can correct find ', function () {
+        var three = _.find([1, 2, 3, 4, 5, 6], function (num) {
+            return num == 3;
+        });
+        expect(three).toBe(3);
+    });
+
+    it('should return undefined if not found', function () {
+        var result = _.find([1, 2, 3, 4, 5, 6], function (num) {
+            return false;
+        });
+        expect(result).toBe(void 0);
+    });
+
+    it('can handle null ', function () {
+        var result = _.find(null, function (num) {
+            return num == 3;
+        });
+        expect(result).toBe(void 0);
+    });
+
+    it('can find obj ', function () {
+        var list = [{a: 1, b: 2}, {a: 2, b: 2}, {a: 1, b: 3}, {a: 1, b: 4}, {a: 2, b: 4}];
+        var result = _.find(list, {a: 1});
+        expect(result).toEqual({a: 1, b: 2});
+    });
+});
+
+
+describe("filter", function () {
+    it('can handle array', function () {
+        var result = _.filter([1, 2, 3, 4, 5, 6], function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toEqual([2, 4, 6]);
+    });
+    it('can handle object', function () {
+        var obj = {one: 1, two: 2, three: 3, four: 4, five: 5, six: 6};
+        var result = _.filter(obj, function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toEqual([2, 4, 6]);
+    });
+    it('can handle null', function () {
+        var result = _.filter(null, function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toEqual([]);
+    });
+
+    it('can handle with context', function () {
+        var result = _.filter([1, 2, 3, 4, 5, 6], function (num) {
+            return num > this.middle;
+        }, {middle: 2});
+        expect(result).toEqual([3, 4, 5, 6]);
+    })
+});
+
+describe("where", function () {
+    it('can handle obj', function () {
+        var listOfPlays = [
+            {title: "Cymbeline", author: "Shakespeare", year: 1611},
+            {title: "Cymbeline", author: "Shakespeare", year: 1612},
+            {title: "The Tempest", author: "Shakespeare", year: 1611}
+        ];
+        var result = _.where(listOfPlays, {author: "Shakespeare", year: 1611});
+        expect(result).toEqual([
+            {title: "Cymbeline", author: "Shakespeare", year: 1611},
+            {title: "The Tempest", author: "Shakespeare", year: 1611}
+        ]);
+    });
+});
+
+describe("findWhere", function () {
+    it('can handle obj', function () {
+        var listOfPlays = [
+            {title: "Cymbeline", author: "Shakespeare", year: 1611},
+            {title: "Cymbeline", author: "Shakespeare", year: 1612},
+            {title: "The Tempest", author: "Shakespeare", year: 1611}
+        ];
+        var result = _.findWhere(listOfPlays, {author: "Shakespeare", year: 1611});
+        expect(result).toEqual(
+            {title: "Cymbeline", author: "Shakespeare", year: 1611}
+        );
+    });
+
+    it('can handle null', function () {
+        var result = _.findWhere(null, {author: "Shakespeare", year: 1611});
+        expect(result).toEqual(void 0);
+    });
+});
+
+
+describe("reject", function () {
+    it('can handle array', function () {
+        var result = _.reject([1, 2, 3, 4, 5, 6], function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toEqual([1, 3, 5]);
+    });
+    it('can handle object', function () {
+        var obj = {one: 1, two: 2, three: 3, four: 4, five: 5, six: 6};
+        var result = _.reject(obj, function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toEqual([1, 3, 5]);
+    });
+    it('can handle null', function () {
+        var result = _.reject(null, function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toEqual([]);
+    });
+
+    it('can handle with context', function () {
+        var result = _.reject([1, 2, 3, 4, 5, 6], function (num) {
+            return num > this.middle;
+        }, {middle: 2});
+        expect(result).toEqual([1, 2]);
+    })
+});
+
+describe("every", function () {
+    it('can handle array', function () {
+        var result = _.every([true, 1, null, 'yes'], _.identity);
+        expect(result).toBe(false);
+    });
+    it('can handle object', function () {
+        var obj = {two: 2, four: 4, six: 6};
+        var result = _.every(obj, function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toBe(true);
+    });
+    it('can handle null', function () {
+        var result = _.every(null, function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toBe(true);
+    });
+
+    it('can handle with context', function () {
+        var result = _.every([3, 4, 5, 6], function (num) {
+            return num > this.middle;
+        }, {middle: 2});
+        expect(result).toBe(true);
+    })
+});
+
+
+describe("some", function () {
+    it('can handle array', function () {
+        var result = _.some([true, 1, null, 'yes'], _.identity);
+        expect(result).toBe(true);
+    });
+    it('can handle object', function () {
+        var obj = {two: 2, four: 4, six: 6};
+        var result = _.some(obj, function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toBe(true);
+    });
+    it('can handle null', function () {
+        var result = _.some(null, function (num) {
+            return num % 2 == 0;
+        });
+        expect(result).toBe(false);
+    });
+
+    it('can handle with context', function () {
+        var result = _.some([3, 4, 5, 6], function (num) {
+            return num < this.middle;
+        }, {middle: 2});
+        expect(result).toBe(false);
+    })
+});
+
+
+describe("contains", function () {
+    it('can handle array', function () {
+        var result = _.contains([1, 2, 3, 4, 5], 3);
+        expect(result).toBe(true);
+    });
+    //obj的时候，是对比value
+    it('can handle object', function () {
+        var obj = {two: 2, four: 4, six: 6};
+        var result = _.contains(obj, 4);
+        expect(result).toBe(true);
+    });
+    it('can handle null', function () {
+        var result = _.contains(null, 1);
+        expect(result).toBe(false);
+    });
+
+    it('can handle with fromIndex', function () {
+        var result1 = _.contains([1, 2, 3, 4, 5], 3, 3);
+        var result2 = _.contains([1, 2, 3, 4, 5], 3, 2);
+        expect(result1).toBe(false);
+        expect(result2).toBe(true);
+    })
 
 });
--- learn_js/learn_lib/learn_underscore/underscore-chen/underscore.js
+++ learn_js/learn_lib/learn_underscore/underscore-chen/underscore.js
@@ -18,9 +18,13 @@
     // common function
     // -------------------
 
-    var getLength = function (obj) {
-        return obj == null ? void 0 : obj["length"];
+    var property = function (key) {
+        return function (obj) {
+            return obj == null ? void 0 : obj[key];
+        };
     };
+
+    var getLength = property('length');
 
     var MAX_ARRAY_INDEX = Math.pow(2, 53) - 1;
     var isArrayLike = function (collection) {
@@ -28,15 +32,57 @@
         return typeof length == 'number' && length >= 0 && length < MAX_ARRAY_INDEX;
     };
 
-    var optimizeCb = function (func, context) {
+
+    var optimizeCb = function (func, context, argCount) {
         if (context === void 0) {
             return func;
         } else {
-            return function (value, index, collection) {
-                func.call(context, value, index, collection);
+            argCount = argCount || 3;
+            switch (argCount) {
+                case 3:
+                    return function (value, index, collection) {
+                        return func.call(context, value, index, collection);
+                    };
+                case 4:
+                    return function (accumulator, value, index, collection) {
+                        return func.call(context, accumulator, value, index, collection);
+                    };
             }
         }
     };
+
+    var cb = function (value, context, argCount) {
+        if (value == null) return _.identity;
+        if (_.isFunction(value)) return optimizeCb(value, context, argCount);
+        if (_.isObject(value)) return _.matcher(value);
+        return _.property(value);
+    };
+
+    _.noop = function () {
+    };
+
+    _.identity = function (value) {
+        return value;
+    };
+
+    _.matcher = function (attrs) {
+        return function (obj) {
+            return _.isMatch(obj, attrs)
+        }
+    };
+
+    _.isMatch = function (object, attrs) {
+        var keys = _.keys(attrs), length = keys.length;
+        if (object == null) return !length;
+        for (var i = 0; i < length; i++) {
+            var key = keys[i];
+            if (attrs[key] !== object[key] || !(key in object)) {
+                return false;
+            }
+        }
+        return true;
+    };
+
 
     // Collection Functions
     // --------------------
@@ -57,13 +103,160 @@
     };
 
     _.map = function (obj, iteratee, context) {
+        iteratee = optimizeCb(iteratee, context);
+        var results = [];
+        _.each(obj, function (value, key, obj) {
+            results[key] = iteratee(value, key, obj);
+        }, context);
+        return results;
+    };
 
-    }
+    var createReduce = function (dir) {
+        var reducer = function (obj, iteratee, memo, initial) {
+            var isArray = isArrayLike(obj);
+            var keys = !isArray && _.keys(obj);
+            var length = (keys || obj).length;
+            var index = dir > 0 ? 0 : length - 1;
+            if (!initial) {
+                memo = obj[isArray ? index : keys[index]];
+                index += dir;
+            }
+            for (; index >= 0 && index < length; index += dir) {
+                var key = isArray ? index : keys[index];
+                memo = iteratee(memo, obj[key], key, obj);
+            }
+            return memo;
+        };
+
+        return function (obj, iteratee, memo, context) {
+            var initial = arguments.length >= 3;
+            iteratee = optimizeCb(iteratee, context, 4);
+            return reducer(obj, iteratee, memo, initial);
+        };
+    };
+
+    _.reduce = createReduce(1);
+
+    _.reduceRight = createReduce(-1);
+
+
+    _.find = function (obj, predicate, context) {
+        predicate = cb(predicate, context);
+        var isArray = isArrayLike(obj);
+        var keys = !isArray && _.keys(obj);
+        var length = (keys || obj).length;
+        var index = 0;
+        for (; index < length; index++) {
+            var key = isArray ? index : keys[index];
+            if (predicate(obj[key], key, obj)) {
+                return obj[key];
+            }
+        }
+        return void 0;
+    };
+
+    _.filter = function (obj, predicate, context) {
+        var results = [];
+        predicate = cb(predicate, context);
+        _.each(obj, function (value, key, list) {
+            if (predicate(value, key, list)) {
+                results.push(value)
+            }
+        });
+        return results;
+    };
+
+    _.where = function (obj, properties) {
+        return _.filter(obj, _.matcher(properties));
+    };
+
+    _.findWhere = function (obj, properties) {
+        return _.find(obj, _.matcher(properties));
+    };
+
+    _.reject = function (list, predicate, context) {
+        predicate = cb(predicate);
+        var rejectPredicate = function () {
+            return !predicate.apply(this, arguments);
+        };
+        return _.filter(list, rejectPredicate, context);
+    };
+
+    _.every = function (obj, predicate, context) {
+        predicate = cb(predicate, context);
+        var keys = !isArrayLike(obj) && _.keys(obj);
+        var length = (keys || obj).length;
+        for (var i = 0; i < length; i++) {
+            var key = keys ? keys[i] : i;
+            if (!predicate(obj[key], key, obj)) return false;
+        }
+        return true;
+    };
+
+    _.some = _.any = function (obj, predicate, context) {
+        predicate = cb(predicate, context);
+        var keys = !isArrayLike(obj) && _.keys(obj),
+            length = (keys || obj).length;
+        for (var index = 0; index < length; index++) {
+            var currentKey = keys ? keys[index] : index;
+            if (predicate(obj[currentKey], currentKey, obj)) return true;
+        }
+        return false;
+    };
+
+    _.contains = function (obj, item, fromIndex) {
+        if (!isArrayLike(obj)) {
+            obj = _.values(obj);
+        }
+        if (typeof fromIndex != 'number') {
+            fromIndex = 0;
+        }
+        return obj.indexOf(item, fromIndex) >= 0;
+    };
+
+    // Array Functions
+    // ------------------------
+
 
     // Object Functions
     // -----------------------
     _.has = function (obj, key) {
         return obj != null && hasOwnProperty.call(obj, key);
     };
+
+
+    _.keys = function (obj) {
+        if (!_.isObject(obj)) {
+            return [];
+        }
+        var keys = [];
+        for (var key in obj) {
+            if (_.has(obj, key)) {
+                keys.push(key);
+            }
+        }
+        return keys;
+    };
+
+    _.values = function (obj) {
+        var keys = _.keys(obj);
+        var length = keys.length;
+        var values = Array(length);
+        for (var i = 0; i < length; i++) {
+            values[i] = (obj[keys[i]]);
+        }
+        return values;
+    };
+
+    _.isObject = function (obj) {
+        var type = typeof obj;
+        return type === "object" || type === 'function' && !!obj;
+    };
+
+    _.isFunction = function (obj) {
+        return typeof obj == 'function';
+    };
+
+    _.property = property;
 
 }());
