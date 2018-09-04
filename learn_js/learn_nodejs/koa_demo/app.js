4a2950fee5ed03412d39e25e6523709dc1a3658d
chen_jia_jia <chenjiajia@zuzuche.com>
2018-09-04 14:38:52 +0800

    koa demo

---
+++ learn_js/learn_nodejs/koa_demo/app.js
@@ -0,0 +1,37 @@
+const Koa = require('koa');
+const app = new Koa();
+
+// logger'
+
+app.context.fuck = 'yes';
+
+app.use(async (ctx, next) => {
+    await next();
+    const rt = ctx.response.get('X-Response-Time');
+    console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+app.use(async (ctx, next) => {
+    console.log(ctx.fuck);
+    throw new Error("test error ");
+    next();
+})
+
+// x-response-time
+app.use(async (ctx, next) => {
+    const start = Date.now();
+    await next();
+    const ms = Date.now() - start;
+    ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+
+app.use(async ctx => {
+    ctx.body = 'hello world';
+});
+
+app.on('error', err => {
+    log.error('server error', err);
+})
+
+app.listen(3000);
