8db4093429114f563a20738dc7cba86cbc74d828
chen_jia_jia <chenjiajia@zuzuche.com>
2018-09-21 19:00:46 +0800

    react native

--- learn_js/learn_react_native/helloWorld/AwesomeProject/App.js
+++ learn_js/learn_react_native/helloWorld/AwesomeProject/App.js
@@ -15,15 +15,19 @@ import {
   Image,
   FlatList
 } from 'react-native';
+import { createBottomTabNavigator, BottomTabBar } from 'react-navigation';
 import moviesJson from './app/json/MoviesExample.json';
 
+const TabBarComponent = (props) => (<View><Text>123123</Text></View>);
+
 type Props = {};
-export default class App extends Component<Props> {
+class App extends Component<Props> {
   constructor(props) {
     super(props); //这一句不能省略，照抄即可
     this.state = {
       data: [],
-      loaded: false
+      loaded: false,
+      refreshing: false
     };
     // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向不对
     // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
@@ -38,7 +42,8 @@ export default class App extends Component<Props> {
     setTimeout(() => {
       this.setState({
         data: this.state.data.concat(moviesJson.movies),
-        loaded: true
+        loaded: true,
+        refreshing: false
       });
     }, 2000);
   }
@@ -67,27 +72,60 @@ export default class App extends Component<Props> {
     );
   }
 
+  keyExtractor(item, index) {
+    return index + '';
+  }
+
+  onRefresh = () => {
+    console.log('on refresh');
+    this.setState({ refreshing: true });
+    this.fetchData();
+  };
+
   render() {
     if (!this.state.loaded) {
       return this.renderLoadingView();
     }
     return (
-      <FlatList
-        data={this.state.data}
-        renderItem={this.renderMovie}
-        style={styles.list}
-      />
+      <View style={styles.app}>
+        <FlatList
+          data={this.state.data}
+          keyExtractor={this.keyExtractor}
+          renderItem={this.renderMovie}
+          style={styles.list}
+          onRefresh={this.onRefresh}
+          refreshing={this.state.refreshing}
+        />
+      </View>
+    );
+  }
+}
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Home!</Text>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    paddingTop: 20
+  },
+  list: {
+    backgroundColor: '#F5FCFF'
+  },
   container: {
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF'
+    backgroundColor: '#F5FCFF',
+    paddingBottom: 20
   },
   rightContainer: {
     flex: 1
@@ -103,9 +141,13 @@ const styles = StyleSheet.create({
   thumbnail: {
     width: 53,
     height: 81
-  },
-  list: {
-    paddingTop: 20,
-    backgroundColor: '#F5FCFF'
   }
 });
+
+let options = {
+  tabBarButtonComponent: props =>(<TabBarComponent {...props} style={{ borderTopColor: '#605F60' }} />)
+};
+export default createBottomTabNavigator({
+  App: App,
+  HomeScreen: HomeScreen,
+},options);
