9afae57ba36df83389b06b9946c5844166dd86be
chen_jia_jia <chenjiajia@zuzuche.com>
2018-09-21 11:51:09 +0800

    react native

---
+++ learn_js/learn_react_native/helloWorld/AwesomeProject/App.js
@@ -0,0 +1,111 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  FlatList
+} from 'react-native';
+import moviesJson from './app/json/MoviesExample.json';
+
+type Props = {};
+export default class App extends Component<Props> {
+  constructor(props) {
+    super(props); //这一句不能省略，照抄即可
+    this.state = {
+      data: [],
+      loaded: false
+    };
+    // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向不对
+    // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    setTimeout(() => {
+      this.setState({
+        data: this.state.data.concat(moviesJson.movies),
+        loaded: true
+      });
+    }, 2000);
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>正在加载电影数据……</Text>
+      </View>
+    );
+  }
+
+  renderMovie(res) {
+    let { item: movie } = res;
+    return (
+      <View style={styles.container}>
+        <Image
+          source={{ uri: movie.posters.thumbnail }}
+          style={styles.thumbnail}
+        />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+
+  render() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+    return (
+      <FlatList
+        data={this.state.data}
+        renderItem={this.renderMovie}
+        style={styles.list}
+      />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  },
+  rightContainer: {
+    flex: 1
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center'
+  },
+  year: {
+    textAlign: 'center'
+  },
+  thumbnail: {
+    width: 53,
+    height: 81
+  },
+  list: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF'
+  }
+});
