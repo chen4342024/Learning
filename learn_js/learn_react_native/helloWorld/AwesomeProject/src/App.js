ff6c9c51a80b018802c58a66772b8cf2eb8779fe
chen_jia_jia <chenjiajia@zuzuche.com>
2018-10-15 10:26:08 +0800

    调整目录结构

--- learn_js/learn_react_native/helloWorld/AwesomeProject/src/App.js
+++ learn_js/learn_react_native/helloWorld/AwesomeProject/src/App.js
@@ -13,16 +13,19 @@ import {
   Text,
   View,
   Image,
-  FlatList
+  FlatList,
+  Button
 } from 'react-native';
-import { createBottomTabNavigator, BottomTabBar } from 'react-navigation';
+import { createBottomTabNavigator, BottomTabBar, createStackNavigator } from 'react-navigation';
 // import moviesJson from './app/json/MoviesExample.json';
-import * as movieService from './src/api/movie'
+import * as movieService from './api/movie';
+import HomeScreen from './HomeScreen'
+import DetailScreen from './DetailScreen'
+import ModalScreen from './ModalScreen'
 
 const TabBarComponent = (props) => (<View><Text>123123</Text></View>);
 
-type Props = {};
-class App extends Component<Props> {
+class App extends Component {
   constructor(props) {
     super(props); //这一句不能省略，照抄即可
     this.state = {
@@ -103,15 +106,6 @@ class App extends Component<Props> {
   }
 }
 
-class HomeScreen extends React.Component {
-  render() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Home!</Text>
-      </View>
-    );
-  }
-}
 
 const styles = StyleSheet.create({
   app: {
@@ -147,9 +141,58 @@ const styles = StyleSheet.create({
 });
 
 let options = {
-  tabBarButtonComponent: props =>(<TabBarComponent {...props} style={{ borderTopColor: '#605F60' }} />)
+  tabBarButtonComponent: props =>(<TabBarComponent {...props} style={{ borderTopColor: '#605F60' }} />),
+  initialRouteName: 'HomeScreen',
+  navigationOptions:{
+    headerStyle: {
+      backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+      fontWeight: 'bold',
+    },
+  }
 };
-export default createBottomTabNavigator({
-  App: App,
-  HomeScreen: HomeScreen,
-},options);
+
+
+const MainStack = createStackNavigator(
+  {
+    HomeScreen: HomeScreen,
+    DetailScreen: {
+      screen:DetailScreen,
+    }
+  },options
+);
+
+MainStack.navigationOptions = ({ navigation }) => {
+  let tabBarVisible = true;
+  if (navigation.state.index > 0) {
+    tabBarVisible = false;
+  }
+  return {
+    tabBarVisible,
+  };
+};
+
+const TabsStack = createBottomTabNavigator(
+  {
+    App: App,
+    Main: MainStack,
+  }
+);
+
+
+const RootStack = createStackNavigator({
+  Tabs: {
+    screen: TabsStack,
+  },
+  MyModal: {
+    screen: ModalScreen,
+  },
+},{
+    mode: 'modal',
+    headerMode: 'none',
+});
+
+export default RootStack;
+
---
+++ learn_js/learn_react_native/helloWorld/AwesomeProject/src/LogoTitle.js
@@ -0,0 +1,36 @@
+
+import React, { Component } from 'react';
+import {
+  Image,
+  View,
+  Text,
+  StyleSheet
+} from 'react-native';
+
+class LogoTitle extends React.Component {
+    render() {
+      return (
+        <View style={styles.title}>
+            <Image
+            source={ require('./assets/logo.png') }
+            style={{ width: 30, height: 30 }}
+            />
+            <Text>test</Text>
+        </View>
+      );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    title: {
+    //   flex: 1,
+    //   paddingTop: 20
+        flexDirection: 'row',
+        alignItems:'center',
+        // justifyContent: 'center',
+        justifyContent: 'space-between',
+    },
+});
+
+export default LogoTitle;
